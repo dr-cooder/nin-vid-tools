9fec3ddb5ae84968283c730669f5380a4516c44f
Felix Warner <fwarner2319@gmail.com>
2026-03-03 23:57:57 -0500

    Create CLI commands

--- src/main.js
+++ src/main.js
@@ -371,25 +371,32 @@ const rebuildDecrypted = (outFilename, yOverride) => {
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 const __cwd = process.cwd();
-if (process.argv.length <= 2) {
-	throw new Error('Input file required');
+if (process.argv.length <= 3) {
+	throw new Error('"extract"/"rebuild" and input file required');
 }
-const inFilePath = process.argv[2];
-// console.log(inFilePath);
+const extractOrRebuild = process.argv[2];
+const inFilePath = process.argv[3];
 const inFilePathFull = path.join(__cwd, inFilePath);
 
-// Uncomment these lines to extract
-/*
-const outFilePathFull = inFilePathFull;
-const inFileData = fs.readFileSync(inFilePathFull);
-const { metadata, subfiles } = extractDecrypted(inFileData);
+let extractMode;
+switch (extractOrRebuild) {
+	case 'extract':
+		extractMode = true;
+		break;
+	case 'rebuild':
+		extractMode = false;
+		break;
+	default:
+		throw new Error(`"${extractOrRebuild}" is not "extract" or "rebuild"`);
+}
+if (extractMode) {
+	const outFilePathFull = inFilePathFull;
+	const inFileData = fs.readFileSync(inFilePathFull);
+	const { metadata, subfiles } = extractDecrypted(inFileData);
 
-fs.writeFileSync(metadataFilename(outFilePathFull), JSON.stringify(metadata, null, '\t')); // TODO: Alphabetize?
-MAIN_SUBFILES.forEach(({ key, filename }) => fs.writeFileSync(filename(outFilePathFull), subfiles[key]));
-subfiles.ads.forEach((adSubfiles, i) => AD_SUBFILES.forEach(({ key, filename }) => fs.writeFileSync(filename(outFilePathFull, i), adSubfiles[key])));
-*/
-
-// Uncomment these lines to rebuild
-/*
-rebuildDecrypted(inFilePathFull);
-*/
+	fs.writeFileSync(metadataFilename(outFilePathFull), JSON.stringify(metadata, null, '\t')); // TODO: Alphabetize?
+	MAIN_SUBFILES.forEach(({ key, filename }) => fs.writeFileSync(filename(outFilePathFull), subfiles[key]));
+	subfiles.ads.forEach((adSubfiles, i) => AD_SUBFILES.forEach(({ key, filename }) => fs.writeFileSync(filename(outFilePathFull, i), adSubfiles[key])));
+} else {
+	rebuildDecrypted(inFilePathFull);
+}
