1594f533058683eb12ae757cf2f2c9e88dc3f031
Felix Warner <fwarner2319@gmail.com>
2026-03-12 23:57:57 -0400

    Created CLI with commander

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@
 // TODO: Rename this file to "nin-vid-tools.js" and export "extraction.js" and "rebuilding.js"
 
 import fs from 'fs';
-import path from 'path';
-// TODO: Actually use command-line-args
-// import commandLineArgs from 'command-line-args';
+// import path from 'path';
+import { program } from 'commander';
 import { keyInYN } from 'readline-sync';
 import { extractDecrypted } from './extraction.js';
 import { rebuildDecrypted } from './rebuilding.js';
@@ -28,13 +27,16 @@ export const readFromFileIfItExists = (filename) => {
 	return data;
 };
 
-// TODO: Make one of these for the "options.json" and "content.bin" seen under the "Decrypted" section of the README
-export const MAIN_SUBFILES = [
+const decryptedOptionsFilename = filename => `${filename}.options.json`;
+
+const decryptedContentFilename = filename => `${filename}.content.bin`;
+
+const MAIN_SUBFILES = [
 	{ key: 'video', filename: filename => `${filename}.video.moflex` },
 	{ key: 'thumbnail', filename: filename => `${filename}.thumb.jpg` }
 ];
 
-export const AD_SUBFILES = [
+const AD_SUBFILES = [
 	{ key: 'image', filename: (filename, index) => `${filename}.ad${index + 1}.jpg` }
 ];
 
@@ -59,86 +61,102 @@ const userApprovesOverwrite = (filenames, description, yOverride) => {
 	const filesToBeOverwrittenCount = filesToBeOverwritten.length;
 
 	return filesToBeOverwrittenCount
-		? keyInYN(`WARNING: The following ${description ? `${description} ` : ''}file${filesToBeOverwrittenCount === 1 ? '' : 's'} will be overwritten:${filesToBeOverwritten.map(filename => `\n\t${filename}`).join('')}\nIs this OK? (this can be overridden with the "-y" option)`)
+		? keyInYN(`WARNING: The following ${description ? `${description} ` : ''}file${filesToBeOverwrittenCount === 1 ? '' : 's'} will be overwritten:${filesToBeOverwritten.map(filename => `\n\t${filename}`).join('')}\nIs this OK? (this can be overridden with the "-y" or "--yes-overwrite" option)`)
 		: true;
 };
 
-const __cwd = process.cwd();
-if (process.argv.length <= 3) {
-	throw new Error('"extract"/"rebuild" and input file required');
-}
-const extractOrRebuild = process.argv[2];
-const inFilePath = process.argv[3];
-const inFilePathFull = path.join(__cwd, inFilePath);
+const generateCommand = ({
+	// program,
+	name,
+	description,
+	argumentIsSource,
+	requiresKey,
+	fn
+}) => (requiresKey
+		? command => command
+			.requiredOption('-k, --boss-aes-key <key>', 'BOSS AES key')
+		: command => command
+	)(program
+		.command(name.replaceAll(' ', '-'))
+		.description(description)
+		.option('-y, --yes-overwrite', `yes, overwrite file${argumentIsSource ? 's' : ''}`))
+	.argument(...(argumentIsSource
+		? ['<source>', `file to ${name} from`]
+		: ['<destination>', `file to ${name} to`]))
+	.action(fn);
 
-let extractMode;
-switch (extractOrRebuild) {
-	case 'extract':
-		extractMode = true;
-		break;
-	case 'rebuild':
-		extractMode = false;
-		break;
-	default:
-		throw new Error(`${JSON.stringify(extractOrRebuild)} is not "extract" or "rebuild"`);
-}
-if (extractMode) {
-	const outFilePathFull = inFilePathFull;
-	const inFileData = fs.readFileSync(inFilePathFull);
-	const { metadata, mainSubfiles, adsSubfiles, dataSectionOddities } = extractDecrypted(inFileData);
-	if (dataSectionOddities) {
-		console.warn(dataSectionOddities);
+generateCommand({
+	name: 'extract',
+	description: 'extract description',
+	argumentIsSource: true,
+	fn: (inFilePath, { yesOverwrite }) => {
+		const outFilePath = inFilePath;
+		const inFileData = fs.readFileSync(inFilePath);
+		const { metadata, mainSubfiles, adsSubfiles, dataSectionOddities } = extractDecrypted(inFileData);
+		if (dataSectionOddities) {
+			console.warn(dataSectionOddities);
+		}
+
+		const filesToWrite = {};
+		filesToWrite[metadataFilename(outFilePath)] = JSON.stringify(metadata, null, '\t');
+		MAIN_SUBFILES.forEach(({ key, filename }) => filesToWrite[filename(outFilePath)] = mainSubfiles[key]);
+		adsSubfiles.forEach((adSubfiles, i) => AD_SUBFILES.forEach(({ key, filename }) => filesToWrite[filename(outFilePath, i)] = adSubfiles[key]));
+		if (userApprovesOverwrite(Object.keys(filesToWrite), 'extracted', yesOverwrite)) {
+			Object.entries(filesToWrite).forEach(value => fs.writeFileSync(...value));
+		}
 	}
+});
 
-	const filesToWrite = {};
-	filesToWrite[metadataFilename(outFilePathFull)] = JSON.stringify(metadata, null, '\t');
-	MAIN_SUBFILES.forEach(({ key, filename }) => filesToWrite[filename(outFilePathFull)] = mainSubfiles[key]);
-	adsSubfiles.forEach((adSubfiles, i) => AD_SUBFILES.forEach(({ key, filename }) => filesToWrite[filename(outFilePathFull, i)] = adSubfiles[key]));
-	if (userApprovesOverwrite(Object.keys(filesToWrite), 'extracted', false)) {
-		Object.entries(filesToWrite).forEach(value => fs.writeFileSync(...value));
-	}
-} else {
-	let metadataHasSyntaxErrors = false;
+generateCommand({
+	name: 'rebuild',
+	description: 'rebuild description',
+	argumentIsSource: false,
+	fn: (outFilePath, { yesOverwrite }) => {
+		let metadataHasSyntaxErrors = false;
 
-	const outMetadataFilename = metadataFilename(inFilePathFull);
-	const metadataData = readFromFileIfItExists(outMetadataFilename);
-	let metadata;
+		const outMetadataFilename = metadataFilename(outFilePath);
+		const metadataData = readFromFileIfItExists(outMetadataFilename);
+		let metadata;
 
-	if (metadataData === undefined) {
-		console.error(`The following file was not found:\n\t${outMetadataFilename}`);
-	} else {
-		try {
-			metadata = JSON.parse(metadataData);
-		} catch (jsonParseError) {
-			if (isType(jsonParseError, SyntaxError)) {
-				metadataHasSyntaxErrors = true;
-			} else {
-				throw jsonParseError;
+		if (metadataData === undefined) {
+			console.error(`The following file was not found:\n\t${outMetadataFilename}`);
+		} else {
+			try {
+				metadata = JSON.parse(metadataData);
+			} catch (jsonParseError) {
+				if (isType(jsonParseError, SyntaxError)) {
+					metadataHasSyntaxErrors = true;
+				} else {
+					throw jsonParseError;
+				}
+			}
+		}
+
+		const mainSubfiles = readSubfiles({
+			subfileSpecs: MAIN_SUBFILES,
+			filenameFunctionParams: [outFilePath]
+		});
+
+		const adsSubfiles = [...Array(metadata?.ads?.length ?? 0).keys().map(i => readSubfiles({
+			subfileSpecs: AD_SUBFILES,
+			filenameFunctionParams: [outFilePath, i]
+		}))];
+
+		if (metadataHasSyntaxErrors) {
+			console.error(`The following file has JSON syntax errors:\n\t${outMetadataFilename}`);
+		} else {
+			let builtBuffer;
+			try {
+				builtBuffer = rebuildDecrypted({ metadata, mainSubfiles, adsSubfiles });
+			} catch (error) {
+				console.error(error.message);
+			}
+			if (builtBuffer && userApprovesOverwrite([outFilePath], 'decrypted', yesOverwrite)) {
+				fs.writeFileSync(outFilePath, builtBuffer);
 			}
 		}
 	}
+});
 
-	const mainSubfiles = readSubfiles({
-		subfileSpecs: MAIN_SUBFILES,
-		filenameFunctionParams: [inFilePathFull]
-	});
-
-	const adsSubfiles = [...Array(metadata?.ads?.length ?? 0).keys().map(i => readSubfiles({
-		subfileSpecs: AD_SUBFILES,
-		filenameFunctionParams: [inFilePathFull, i]
-	}))];
-
-	if (metadataHasSyntaxErrors) {
-		console.error(`The following file has JSON syntax errors:\n\t${outMetadataFilename}`);
-	} else {
-		let builtBuffer;
-		try {
-			builtBuffer = rebuildDecrypted({ metadata, mainSubfiles, adsSubfiles });
-		} catch (error) {
-			console.error(error.message);
-		}
-		if (builtBuffer && userApprovesOverwrite([inFilePathFull], 'decrypted', false)) {
-			fs.writeFileSync(inFilePathFull, builtBuffer);
-		}
-	}
-}
+program
+	.parse(process.argv);
